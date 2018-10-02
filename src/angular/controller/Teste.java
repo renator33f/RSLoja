104a8316abfe861f51c5e1476751d48009e576df
renator33f <39055204+renator33f@users.noreply.github.com>
2018-10-02 12:25:29 -0300

    V1-2

---
+++ src/angular/controller/Teste.java
@@ -0,0 +1,17 @@
+package angular.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import curso.angular.model.PedidoBean;
+
+public class Teste {
+
+	
+	public static void main(String[] args) {
+		String json = "{\"pedido\":{\"valorTotal\":\"R$970.00\",\"data\":\"\"}}";
+		
+		PedidoBean pedidoBean = new Gson().fromJson(json,
+				PedidoBean.class);
+	}
+}
